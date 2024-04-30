869841de787bda43bab3b15ba8084c5c2d7bc282
MolehLuka <luka.moleh@student.um.si>
2024-05-01 00:56:04 +0200

    #12 - introduction page design change - not final

--- src/components/introduction/IntroductionContent.tsx
+++ src/components/introduction/IntroductionContent.tsx
@@ -1,51 +1,24 @@
 import { Link } from "react-router-dom";
 import Paths from "../../util/Paths";
+import IntroductionSlider from "./IntroductionSlider";
 
 export default function IntroductionContent() {
   return (
-    <div className="flex px-16 flex-grow">
-      <div className="flex flex-col w-2/5 justify-center text-6xl space-y-24 font-bold">
+    <div className="flex h-screen items-center px-16">
+      <div className="flex flex-col w-2/5 justify-center text-5xl space-y-24 font-bold">
         <h1>Master the Art of Project Management</h1>
         <h1>Where Goals Meet Execution</h1>
         <h1>The Ultimate Project Command Center</h1>
       </div>
-      <div className="flex flex-col w-3/5 justify-center space-y-12">
-        <div className="flex justify-center text-4xl font-semibold">
+      <div className="flex flex-col w-3/5 justify-center space-y-12 bg-[#0E1428]/40 h-full">
+        <div className="flex justify-center text-4xl font-semibold text-white">
           <h1>Features</h1>
         </div>
-        <div className="">
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-        </div>
-        <div className="">
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?"
-          </p>
-        </div>
+        <IntroductionSlider />
         <div className="flex justify-center">
           <Link to={Paths.SIGN_UP} className="w-1/2">
-            <div className="p-4 bg-black text-white flex justify-center rounded-lg font-semibold">
-              Sign Up
+            <div className="p-4 bg-white text-black flex justify-center rounded-lg font-semibold">
+              Start managing now 
             </div>
           </Link>
         </div>
--- src/components/introduction/IntroductionHeader.tsx
+++ src/components/introduction/IntroductionHeader.tsx
@@ -3,15 +3,15 @@ import Paths from "../../util/Paths";
 
 export default function IntroductionHeader() {
   return (
-    <div className="flex flex-col sticky top-0 backdrop-blur-lg ">
+    <div className="flex flex-col">
       <div className="flex flex-row px-10 py-6 items-center">
-        <div className="flex-auto">
+        <div className="flex-auto w-2/5">
           <div className="flex flex-row space-x-6 ">
             <div>icon</div>
             <div className="text-xl font-semibold">ProjectManager</div>
           </div>
         </div>
-        <Link to={Paths.SIGN_IN} className="w-24">
+        <Link to={Paths.SIGN_IN} className="flex w-3/5 justify-end">
           <div className="flex justify-center p-2 text-black font-semibold hover:text-gray-800">
             Sign In
           </div>
