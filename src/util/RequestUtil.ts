b55d912664bdc2ab0a2ea985447dddf795d01f28
FrostyKitten02 <alen.fridau02@gmail.com>
2024-05-15 22:50:40 +0200

    #0000 - deploy fix

--- src/util/RequestUtil.ts
+++ src/util/RequestUtil.ts
@@ -4,6 +4,7 @@ import {RawAxiosRequestConfig} from "axios";
 export default class RequestUtil {
     private constructor() {}
     public static createBaseAxiosRequestConfig(session: string): RawAxiosRequestConfig {
+        console.log(import.meta.env.VITE_BACKEND_BASE_URL)
         return {
             baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
             headers: {
