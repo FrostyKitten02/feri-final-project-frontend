04e5955b16e6b72c8cc9368fad2662f3b2f32f20
klarakeb <klara.kebric@student.um.si>
2024-05-18 23:01:07 +0200

    #43 - routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import {Navigate, Outlet, useLocation} from "react-router-dom";
-import {SignedIn, SignedOut,} from "@clerk/clerk-react";
-import Paths from "./util/Paths";
+import {Outlet} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 
 function App() {
-    const location = useLocation();
-    const {pathname} = location;
     return (
         <div>
-            <SignedIn>
-                <Outlet/>
-            </SignedIn>
-            <SignedOut>
-                {
-                    pathname === "/" ? <Navigate to={Paths.INTRODUCTION} replace={true}/> : null
-                }
-                <Outlet/>
-            </SignedOut>
+            <Outlet/>
             <ToastContainer/>
         </div>
     )
