24dfbe6470afd112406e391e205b55f924cc76ea
MolehLuka <luka.moleh@student.um.si>
2024-05-19 23:11:04 +0200

    #48 - client update for user type

--- temp_ts/api.ts
+++ temp_ts/api.ts
@@ -39,6 +39,31 @@ export interface AddPersonToProjectRequest {
 /**
  * 
  * @export
+ * @interface CreatePersonTypeRequest
+ */
+export interface CreatePersonTypeRequest {
+    /**
+     * 
+     * @type {string}
+     * @memberof CreatePersonTypeRequest
+     */
+    'name'?: string;
+    /**
+     * 
+     * @type {number}
+     * @memberof CreatePersonTypeRequest
+     */
+    'researchAvailability'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof CreatePersonTypeRequest
+     */
+    'educateAvailability'?: number;
+}
+/**
+ * 
+ * @export
  * @interface CreateProjectRequest
  */
 export interface CreateProjectRequest {
@@ -575,6 +600,112 @@ export class PersonControllerApi extends BaseAPI {
      */
     public getPersonById(personId: string, options?: RawAxiosRequestConfig) {
         return PersonControllerApiFp(this.configuration).getPersonById(personId, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+
+/**
+ * PersonTypeControllerApi - axios parameter creator
+ * @export
+ */
+export const PersonTypeControllerApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {CreatePersonTypeRequest} createPersonTypeRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createPersonType: async (createPersonTypeRequest: CreatePersonTypeRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'createPersonTypeRequest' is not null or undefined
+            assertParamExists('createPersonType', 'createPersonTypeRequest', createPersonTypeRequest)
+            const localVarPath = `/person-type`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(createPersonTypeRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * PersonTypeControllerApi - functional programming interface
+ * @export
+ */
+export const PersonTypeControllerApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = PersonTypeControllerApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * 
+         * @param {CreatePersonTypeRequest} createPersonTypeRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async createPersonType(createPersonTypeRequest: CreatePersonTypeRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<ResourceCreatedResponse>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.createPersonType(createPersonTypeRequest, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['PersonTypeControllerApi.createPersonType']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * PersonTypeControllerApi - factory interface
+ * @export
+ */
+export const PersonTypeControllerApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = PersonTypeControllerApiFp(configuration)
+    return {
+        /**
+         * 
+         * @param {CreatePersonTypeRequest} createPersonTypeRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createPersonType(createPersonTypeRequest: CreatePersonTypeRequest, options?: any): AxiosPromise<ResourceCreatedResponse> {
+            return localVarFp.createPersonType(createPersonTypeRequest, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * PersonTypeControllerApi - object-oriented interface
+ * @export
+ * @class PersonTypeControllerApi
+ * @extends {BaseAPI}
+ */
+export class PersonTypeControllerApi extends BaseAPI {
+    /**
+     * 
+     * @param {CreatePersonTypeRequest} createPersonTypeRequest 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof PersonTypeControllerApi
+     */
+    public createPersonType(createPersonTypeRequest: CreatePersonTypeRequest, options?: RawAxiosRequestConfig) {
+        return PersonTypeControllerApiFp(this.configuration).createPersonType(createPersonTypeRequest, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
