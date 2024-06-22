136bf3b7f2d0ffb3fb36b66228ae8863d943c189
MolehLuka <luka.moleh@student.um.si>
2024-06-22 14:09:53 +0200

    #97 - design update

--- src/components/project-main/team/ProjectTeamPage.tsx
+++ src/components/project-main/team/ProjectTeamPage.tsx
@@ -6,7 +6,6 @@ import { toastError } from "../../toast-modals/ToastFunctions";
 import { projectAPI } from "../../../util/ApiDeclarations";
 import { useRequestArgs } from "../../../util/CustomHooks";
 import { BsPersonDash } from "react-icons/bs";
-import { HiOutlineUserGroup } from "react-icons/hi2";
 
 export default function ProjectTeamPage() {
   const [peopleOnProject, setPeopleOnProject] = useState<PersonDto[]>([]);
@@ -41,38 +40,43 @@ export default function ProjectTeamPage() {
         <div
           className={`flex flex-col border-r-2 border-solid border-gray-200 w-full h-full`}
         >
-          <div className="flex flex-row items-center p-10 text-xl font-semibold gap-x-4">
-            <HiOutlineUserGroup className="stroke-black size-8" />
+          <div className="flex flex-row items-center p-10 text-2xl font-semibold border-b border-solid border-gray-200">
             <p>Employee list</p>
           </div>
-          <div className="w-full h-full">
-            <div className="grid grid-cols-3 border-b-2 border-solid border-gray-200 py-6">
+          <div className="w-full h-full px-12 bg-gray-200 rounded-bl-[20px]">
+            <div className="grid grid-cols-3 pt-8 pb-4">
               <div className="flex justify-center items-center gap-x-4">
-                <div className="text-sm text-gray-500 font-semibold">Name</div>
+                <div className="text-sm text-gray-600 font-semibold">NAME</div>
               </div>
               <div className="flex justify-center items-center gap-x-4">
-                <div className="text-sm text-gray-500 font-semibold">Email</div>
+                <div className="text-sm text-gray-600 font-semibold">
+                  EMAIL ADDRESS
+                </div>
               </div>
               <div></div>
             </div>
-            {peopleOnProject?.map((person) => (
-              <div
-                className="grid grid-cols-3 border-b-2 border-solid border-gray-200 py-6 hover:bg-gray-200 transition delay-50"
-                key={person.id}
-              >
-                <div className="flex items-center justify-center text-md font-semibold">
-                  {person.name} {person.lastname}
+            <div className="rounded-2xl border border-solid border-gray-200 overflow-hidden bg-white shadow-md divide-y divide-solid divide-gray-200">
+              {peopleOnProject?.map((person) => (
+                <div
+                  className="grid grid-cols-3 py-6 hover:bg-gray-100 transition delay-50"
+                  key={person.id}
+                >
+                  <div className="flex items-center justify-center text-sm font-semibold">
+                    <div>
+                      {person.name} {person.lastname}
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center text-sm font-normal text-gray-500">
+                    {person.email}
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <button>
+                      <BsPersonDash className="size-6 fill-gray-500 hover:fill-red-500 transition delay-50" />
+                    </button>
+                  </div>
                 </div>
-                <div className="flex items-center justify-center text-md font-semibold">
-                  {person.email}
-                </div>
-                <div className="flex items-center justify-center">
-                  <button>
-                    <BsPersonDash className="size-6 fill-gray-500 hover:fill-red-500 transition delay-50" />
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
         <div className="flex px-6 items-start pt-6">
