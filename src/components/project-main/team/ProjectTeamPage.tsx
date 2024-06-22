eead1534d5ad8a733d5e35a89029c34a15165aac
MolehLuka <luka.moleh@student.um.si>
2024-06-22 14:23:33 +0200

    #97 - minor fix

--- src/components/project-main/team/ProjectTeamPage.tsx
+++ src/components/project-main/team/ProjectTeamPage.tsx
@@ -44,39 +44,54 @@ export default function ProjectTeamPage() {
             <p>Employee list</p>
           </div>
           <div className="w-full h-full px-12 bg-gray-200 rounded-bl-[20px]">
-            <div className="grid grid-cols-3 pt-8 pb-4">
-              <div className="flex justify-center items-center gap-x-4">
-                <div className="text-sm text-gray-600 font-semibold">NAME</div>
-              </div>
-              <div className="flex justify-center items-center gap-x-4">
-                <div className="text-sm text-gray-600 font-semibold">
-                  EMAIL ADDRESS
-                </div>
-              </div>
-              <div></div>
-            </div>
-            <div className="rounded-2xl border border-solid border-gray-200 overflow-hidden bg-white shadow-md divide-y divide-solid divide-gray-200">
-              {peopleOnProject?.map((person) => (
-                <div
-                  className="grid grid-cols-3 py-6 hover:bg-gray-100 transition delay-50"
-                  key={person.id}
-                >
-                  <div className="flex items-center justify-center text-sm font-semibold">
-                    <div>
-                      {person.name} {person.lastname}
+            {peopleOnProject.length > 0 ? (
+              <>
+                <div className="grid grid-cols-3 pt-8 pb-4">
+                  <div className="flex justify-center items-center gap-x-4">
+                    <div className="text-sm text-gray-600 font-semibold">
+                      NAME
                     </div>
                   </div>
-                  <div className="flex items-center justify-center text-sm font-normal text-gray-500">
-                    {person.email}
+                  <div className="flex justify-center items-center gap-x-4">
+                    <div className="text-sm text-gray-600 font-semibold">
+                      EMAIL ADDRESS
+                    </div>
                   </div>
-                  <div className="flex items-center justify-center">
-                    <button>
-                      <BsPersonDash className="size-6 fill-gray-500 hover:fill-red-500 transition delay-50" />
-                    </button>
-                  </div>
+                  <div></div>
                 </div>
-              ))}
-            </div>
+                <div className="rounded-2xl border border-solid border-gray-200 overflow-hidden bg-white shadow-md divide-y divide-solid divide-gray-200">
+                  {peopleOnProject?.map((person) => (
+                    <div
+                      className="grid grid-cols-3 py-6 hover:bg-gray-100 transition delay-50"
+                      key={person.id}
+                    >
+                      <div className="flex items-center justify-center text-sm font-semibold">
+                        <div>
+                          {person.name} {person.lastname}
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-center text-sm font-normal text-gray-500">
+                        {person.email}
+                      </div>
+                      <div className="flex items-center justify-center">
+                        <button>
+                          <BsPersonDash className="size-6 fill-gray-500 hover:fill-red-500 transition delay-50" />
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col h-full items-center justify-center">
+                <p className="text-2xl font-semibold">
+                  There is no one currently assigned to this project.
+                </p>
+                <p>
+                  Navigate to the top right to assign someone to this project.
+                </p>
+              </div>
+            )}
           </div>
         </div>
         <div className="flex px-6 items-start pt-6">
