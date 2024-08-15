ac8c0d9cb33a5086eba225db1f963e56704b1576
MolehLuka <74741071+MolehLuka@users.noreply.github.com>
2024-08-16 00:59:41 +0200

    #151 - updated landing page design (not final) (#153)
    * #151 - updated landing page design (not final)
    
    * #151 - removed unused import

--- src/components/authorization/SignInPage.tsx
+++ src/components/authorization/SignInPage.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 function SignInPage() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen overflow-auto bg-gradient-to-r from-bg_placeholder1 to-bg_placeholder2">
+    <div className="flex justify-center items-center w-screen h-screen overflow-auto bg-gradient-to-tr from-c-sky to-white">
       <SignIn signUpUrl={import.meta.env.VITE_SIGN_UP_URL} />
     </div>
   );
--- src/components/authorization/SignUpPage.tsx
+++ src/components/authorization/SignUpPage.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 function SignUpPage() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen overflow-auto bg-gradient-to-r from-bg_placeholder1 to-bg_placeholder2">
+    <div className="flex justify-center items-center w-screen h-screen overflow-auto bg-gradient-to-tr from-c-sky to-white">
       <SignUp signInUrl={import.meta.env.VITE_SIGN_IN_URL}/>
     </div>
   );
