03fbb4908ab0688b67daeb88b7d24765c828a426
klarakeb <klara.kebric@student.um.si>
2024-08-21 21:45:04 +0200

    #176 - error page design

--- src/components/template/pages/ErrorPage.tsx
+++ src/components/template/pages/ErrorPage.tsx
@@ -1,23 +1,32 @@
 import {Link} from "react-router-dom";
 import Paths from "../../../util/Paths";
+import steerLogoPath from "../../../assets/images/steer_logo_black.png";
 
 function ErrorPage() {
-  return (
-    <div className="flex flex-col h-full justify-center items-start space-y-6 px-32 overflow-auto bg-gradient-to-r from-bg_placeholder1 to-bg_placeholder2">
-      <div className="flex justify-center w-24 py-1 bg-gray-300 font-medium text-slate-500 border border-solid border-slate-500 rounded-lg">
-            AMEJZING ERROR
-      </div>
-      <div className="text-5xl font-bold">Oops! Something went wrong.. 😢</div>
-      <div className="text-xl">
-        The page you are looking for does not exist or is temporarily
-        unavailable.
-      </div>
-      <Link to={Paths.HOME} className="w-40">
-        <div className="p-3 bg-black text-white flex justify-center rounded-lg font-semibold">
-          Go Back
+    return (
+        <div className="px-32 py-5 h-screen bg-gradient-to-tr from-white to-c-sky flex flex-col">
+            <Link to={Paths.HOME}>
+                <img src={steerLogoPath} alt="Steer" className="h-24"/>
+            </Link>
+            <div className="space-y-6 h-full flex flex-col justify-center">
+                <div className="text-8xl font-bold">
+                    404
+                </div>
+                <div className="text-5xl font-bold">
+                    Oops! Page not Found
+                </div>
+                <div className="text-xl">
+                    The page you are looking for does not exist or is temporarily
+                    unavailable.
+                </div>
+                <Link to={Paths.HOME} className="w-40">
+                    <div className="p-3 bg-black text-white flex justify-center rounded-lg font-semibold">
+                        Go Back
+                    </div>
+                </Link>
+            </div>
         </div>
-      </Link>
-    </div>
-  );
+    );
 }
+
 export default ErrorPage;
