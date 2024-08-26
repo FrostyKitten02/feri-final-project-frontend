1e47b944a5b15dc7cb88c915f8004437fe3825be
MolehLuka <luka.moleh@student.um.si>
2024-08-26 12:38:19 +0200

    #202 - removed sign up from landing page and form, added custom icon and text to button

---
+++ src/components/template/sign-in-icon/CustomIcon.tsx
@@ -0,0 +1,14 @@
+import umIcon from "../../../assets/images/um_icon.png";
+
+export const CustomIcon = () => {
+  return (
+    <img
+      src={umIcon}
+      alt="UM Icon"
+      style={{
+        width: "50px",
+        height: "50px",
+      }}
+    />
+  );
+};
