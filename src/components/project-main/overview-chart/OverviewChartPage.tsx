c31faa498c4587165857f2bda94b527caceac4e8
MolehLuka <luka.moleh@student.um.si>
2024-08-28 02:50:08 +0200

    #223 - removed imports

--- src/components/project-main/overview-chart/OverviewChartPage.tsx
+++ src/components/project-main/overview-chart/OverviewChartPage.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {ProjectStatisticsResponse, WorkPackageDto} from "../../../../client";
+import {ProjectStatisticsResponse} from "../../../../client";
 import {projectAPI} from "../../../util/ApiDeclarations";
 import {useRequestArgs} from "../../../util/CustomHooks";
 import {useParams} from "react-router-dom";
