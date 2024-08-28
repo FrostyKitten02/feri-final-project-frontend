007d4c7b964aefaf8e6981b0a7c801a9542c9584
MolehLuka <luka.moleh@student.um.si>
2024-08-28 16:30:35 +0200

    #236 - grid fix

--- src/components/project-main/team/ProjectTeamPage.tsx
+++ src/components/project-main/team/ProjectTeamPage.tsx
@@ -58,8 +58,9 @@ export default function ProjectTeamPage() {
             </div>
           ) : peopleOnProject.length > 0 ? (
             <div className="flex flex-col h-full">
-              <div className="grid grid-cols-[1fr_1fr_1fr_0.5fr_0.5fr] pt-8 pb-4 px-3">
-                <div className="flex justify-center items-center gap-x-4">
+              <div className="grid grid-cols-[80px_1fr_1fr_1fr_0.5fr_0.5fr] pt-8 pb-4 px-3">
+                <div />
+                <div className="flex justify-start items-center gap-x-4">
                   <div className="text-sm text-gray-600 font-semibold">
                     NAME
                   </div>
@@ -89,47 +90,45 @@ export default function ProjectTeamPage() {
                 <div className="rounded-2xl border border-solid border-gray-200 bg-white divide-y divide-solid divide-gray-200">
                   {peopleOnProject?.map((person) => (
                     <div
-                      className="grid grid-cols-[1fr_1fr_1fr_0.5fr_0.5fr] py-6"
+                      className="grid grid-cols-[80px_1fr_1fr_1fr_0.5fr_0.5fr] py-6"
                       key={person.id}
                     >
-                      <div className="flex items-center justify-center text-sm font-semibold gap-x-4">
-                        <div>
-                          {person.profileImageUrl ? (
-                            <>
-                              {!userImageLoaded[person.id as string] && (
-                                <div className="rounded-full animate-pulse h-8 w-8 bg-slate-200" />
-                              )}
-                              <img
-                                src={person.profileImageUrl}
-                                className={`${
-                                  userImageLoaded[person.id as string]
-                                    ? "block"
-                                    : "hidden"
-                                } size-8 rounded-full`}
-                                onLoad={() => updateImageState(person.id)}
-                              />
-                            </>
-                          ) : (
+                      <div className="flex justify-end items-center">
+                        {person.profileImageUrl ? (
+                          <>
+                            {!userImageLoaded[person.id as string] && (
+                              <div className="rounded-full animate-pulse h-8 w-8 bg-slate-200" />
+                            )}
                             <img
-                              src={ClerkDefaultImg}
-                              className="size-8 rounded-full"
+                              src={person.profileImageUrl}
+                              className={`${
+                                userImageLoaded[person.id as string]
+                                  ? "block"
+                                  : "hidden"
+                              } size-8 rounded-full`}
+                              onLoad={() => updateImageState(person.id)}
                             />
-                          )}
-                        </div>
-                        <div>
+                          </>
+                        ) : (
+                          <img
+                            src={ClerkDefaultImg}
+                            className="size-8 rounded-full"
+                          />
+                        )}
+                      </div>
+                      <div className="flex items-center px-4 justify-start text-sm font-semibold overflow-hidden">
                           {person.name && person.lastname ? (
-                            <p>
+                            <p className="truncate">
                               {person.name} {person.lastname}
                             </p>
                           ) : (
                             <p>N/A</p>
                           )}
-                        </div>
                       </div>
                       <div className="flex items-center justify-center text-sm font-normal text-gray-500">
                         {person.email}
                       </div>
-                      <div className="flex flex-col items-center justify-center text-sm">
+                      <div className="flex flex-col items-center justify-center text-center text-sm">
                         <div className="font-semibold">
                           {TextUtil.refactorDate(person.fromDate)} -{" "}
                           {TextUtil.refactorDate(person.toDate)}{" "}
